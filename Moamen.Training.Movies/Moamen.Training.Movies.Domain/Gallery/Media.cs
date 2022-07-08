e11e3da117312c3f315a037c3f25955ad5ae8a25
Moamen Shehata <engmoamen00@gmail.com>
2022-07-08 09:56:36 +0200

    Fixed Media Entity to have a direct column for MediaType instead of one:one relation

--- Moamen.Training.Movies/Moamen.Training.Movies.Domain/Gallery/Media.cs
+++ Moamen.Training.Movies/Moamen.Training.Movies.Domain/Gallery/Media.cs
@@ -7,12 +7,11 @@ using System.Threading.Tasks;
 
 namespace Moamen.Training.Movies.Domain.Gallery
 {
-    public class Media : IHasOneMediaRole<MediaRole, int>, IHasOneMovie<Movie, int>
+    public class Media : IHasOneMovie<Movie, int>
     {
         public int Id { get; set; }
         public string Url { get; set; }
-        public int RoleId { get; set; }
-        public MediaRole Role { get; set; }
+        public MediaRoleType MediaType { get; set; }
         public int MovieId { get; set; }
         public Movie Movie { get; set; }
     }
