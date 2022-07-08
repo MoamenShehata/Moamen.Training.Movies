2ec29114120bc51121da5a13edd4c880954ef925
Moamen Shehata <engmoamen00@gmail.com>
2022-07-08 23:46:45 +0200

    Added Movies Controller with single action for retrieving All Movies and all its required services

---
+++ Moamen.Training.Movies/Moamen.Training.Movies.API/Infrastrcuture/ApplicationBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+
+namespace Moamen.Training.Movies.API.Infrastrcuture
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseAutofacServiceProvider(this WebApplicationBuilder builder)
+        {
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+        }
+
+        public static void RegisterComponents(
+            this WebApplicationBuilder builder,
+            string connectionString)
+        {
+            builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterDependancies(connectionString);
+            });
+        }
+
+
+    }
+}
